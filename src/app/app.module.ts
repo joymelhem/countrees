b3da3b111aaa3827fe86963f4205906b7392aa64
joymelhem <joymelhem31@gmail.com>
2023-08-09 16:35:31 +0300

    fixes + progress on main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,8 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatIconModule} from '@angular/material/icon'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
--- src/app/main-page/country-list/country-list.component.ts
+++ src/app/main-page/country-list/country-list.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+interface Country {
+  name: string;
+  flag: string;
+  continent: string;
+}
+
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
   styleUrls: ['./country-list.component.scss']
 })
 export class CountryListComponent {
-
+  countries: Country[] = [
+    {
+      name: 'USA',
+      flag: 'https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png', 
+      continent: 'North America'
+    },
+    {
+      name: 'China',
+      flag: 'https://www.countryflags.com/wp-content/uploads/china-flag-png-large.png', 
+      continent: 'Asia'
+    },
+    {
+      name: 'Japan',
+      flag: 'https://www.countryflags.com/wp-content/uploads/japan-flag-png-large.png',
+      continent: 'Asia'
+    },
+    {
+      name: 'Germany',
+      flag: 'https://www.countryflags.com/wp-content/uploads/germany-flag-png-large.png', 
+      continent: 'Europe'
+    },
+    {
+      name: 'Egypt',
+      flag: 'https://www.countryflags.com/wp-content/uploads/egypt-flag-png-large.png', 
+      continent: 'Africa'
+    },
+    {
+      name: 'United Kingdom',
+      flag: 'https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png',
+      continent: 'Europe'
+    },
+  ];
 }
